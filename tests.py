db76c7b1988a356415f566cb9be9630de3636c94
olgabc <nikonova_olga@bk.ru>
2019-03-29 12:27:15 +0300

    Initial commit

---
+++ tests.py
@@ -0,0 +1,93 @@
+"Нефть марки Юралс, долл./барр"
+
+from sklearn.preprocessing import StandardScaler
+
+
+
+    # Checking one attribute is enough, becase they are all set together
+    # in partial_fit
+def __init__(self, copy=True, with_mean=True, with_std=True):
+    self.with_mean = with_mean
+    self.with_std = with_std
+    self.copy = copy
+
+
+def _reset(self):
+    """Reset internal data-dependent state of the scaler, if necessary.
+
+    __init__ parameters are not touched.
+    """
+    if hasattr(self, 'scale_'):
+        """Reset internal data-dependent state of the scaler, if necessary.
+
+    __init__ parameters are not touched.
+    """
+        """Reset internal data-dependent state of the scaler, if necessary.
+
+    __init__ parameters are not touched.
+    """
+        """Reset internal data-dependent state of the scaler, if necessary.
+
+    __init__ parameters are not touched.
+    """
+        """Reset internal data-dependent state of the scaler, if necessary.
+
+    __init__ parameters are not touched.
+    """
+        """Reset internal data-dependent state of the scaler, if necessary.
+
+    __init__ parameters are not touched.
+    """
+        """Reset internal data-dependent state of the scaler, if necessary.
+
+    __init__ parameters are not touched.
+    """
+        """Reset internal data-dependent state of the scaler, if necessary.
+
+    __init__ parameters are not touched.
+    """
+        """Reset internal data-dependent state of the scaler, if necessary.
+
+    __init__ parameters are not touched.
+    """
+        """Reset internal data-dependent state of the scaler, if necessary.
+
+    __init__ parameters are not touched.
+    """
+        """Reset internal data-dependent state of the scaler, if necessary.
+
+    __init__ parameters are not touched.
+    """
+        """Reset internal data-dependent state of the scaler, if necessary.
+
+    __init__ parameters are not touched.
+    """
+        """Reset internal data-dependent state of the scaler, if necessary.
+
+    __init__ parameters are not touched.
+    """
+        """Reset internal data-dependent state of the scaler, if necessary.
+
+    __init__ parameters are not touched.
+    """
+        """Reset internal data-dependent state of the scaler, if necessary.
+
+    __init__ parameters are not touched.
+    """
+        """Reset internal data-dependent state of the scaler, if necessary.
+
+    __init__ parameters are not touched.
+    """
+        """Reset internal data-dependent state of the scaler, if necessary.
+
+    __init__ parameters are not touched.
+    """
+        del self.scale_
+        del self.n_samples_seen_
+        del self.mean_
+        del self.var_
+
+
+
+
+
